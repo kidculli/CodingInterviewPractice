4d6e2cf217e788cadb504e34504bed573d2964c1
kidculli <cullinnotedward@gmail.com>
2018-02-27 23:33:17 -0500

    updated rotate 90 ignore files

--- Chapter1ArrayStrings/1_6.py
+++ Chapter1ArrayStrings/1_6.py
@@ -1,10 +1,12 @@
+import numpy as np
+import pdb 
 def prettyprintmatrix(matrix):
     for row in matrix:
         for col in row:
             print col,
         print 
-the_matrix = [[0,0,1],[1,1,1],[1,0,0]] 
-
+the_matrix = [[21,0,22,73,213],[1,1,1,45,96],[24,0,23,87,99],[24,0,23,87,99],[24,0,23,87,99]] 
+#the_matrix = [[1,2,3],[4,5,6],[7,8,9]]
 '''
 rotates a matrix 90 degrees 
 O(n) 
@@ -24,4 +26,51 @@ def rotateMatrix90(matrix):
             if rotated_rows < 0:
                 break
     prettyprintmatrix(rotated)
-rotateMatrix90(the_matrix)
+
+
+"""
+@param matrix - 2d matrix of ints 
+@n - length of each side 
+"""
+def rotateMatrixInPlace(matrix, n):
+    arr = np.array(matrix)
+    current = arr[0][0]
+    print arr
+    temp = arr[0][n-1]
+    cycles = n/2
+    for cycle in range(0,cycles):
+        # begging column index for this cycle 
+        begin = 0 + cycle
+        # ending column index for this cycle
+        end = n-1-cycle
+
+        for i in range(begin, end):
+            # top left 
+            current = arr[begin,i]
+            # top right 
+            temp = arr[i,end]
+            arr[i,end] = current
+            #swap 
+            current = temp
+
+            #bottom right 
+            temp = arr[end][end - i]
+            arr[end][end - i] = current
+            #swap 2 
+            current = temp
+            
+            #bottom left 
+            temp = arr[end - i][begin]
+            arr[end - i][begin] = current 
+            #swap 3 
+
+            #top left 
+            arr[begin][i] = temp 
+            #swap 4 
+        print "----------------------------------"
+        print arr
+        
+
+if __name__ == "__main__":
+    #rotateMatrix90(the_matrix)
+    rotateMatrixInPlace(the_matrix,len(the_matrix[0]))
